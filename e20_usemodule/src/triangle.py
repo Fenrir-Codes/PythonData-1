5984676ed50f9ceebba7115a5bfb7877e4be43c9
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2023-04-26 13:13:08 +0200

    Add files via upload

---
+++ e20_usemodule/src/triangle.py
@@ -0,0 +1,13 @@
+# Enter you module contents here
+'docstring of triangle'
+__author__ = "author"
+__version__ = "1"
+import math
+
+def hypothenuse(a, b):
+    "hypothenuse string"
+    return math.sqrt(a**2 + b**2)
+
+def area(a, b):
+    """area string"""
+    return a * b / 2
